a8c72d1eaff2deeea92224bf71792f06bbf6e756
Harshil <harshil@unified.com>
2026-08-12 10:31:27 +0530

    Add Google Tag Manager snippet

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,5 @@
 import { Suspense } from "react";
+import Script from "next/script";
 import "@/app/globals.css";
 import AppBlurWrapper from "@/components/AppBlurWrapper";
 import Navbar from "@/components/Navbar";
@@ -122,6 +123,23 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body className="antialiased bg-white text-slate-900" suppressHydrationWarning>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NKN7BMWF"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-NKN7BMWF');
+          `}
+        </Script>
         <Suspense fallback={null}>
           <AnalyticsTracker />
         </Suspense>
